a4b2dd4bb2b9e3527b935c71bdf844fddfed55a6
Rob Parker <into.the.parker@gmail.com>
2018-09-23 09:51:48 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,9 @@
 def alphabetize(arr)
  if arr == ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
-   arr = 
+   arr = ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
+ else 
+   arr.sort |a,b|
+    a<=>b
+  end
+  arr
 end
