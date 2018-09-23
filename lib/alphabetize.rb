d1c00917edaf340ecd9157501f6b8a5cf9ea0838
Rob Parker <into.the.parker@gmail.com>
2018-09-23 09:51:05 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,20 +1,3 @@
 def alphabetize(arr)
-if arr[3] == 'ĉu vi parolas esperanton'
-    arr[3] = 'cu vi parolas esperanton'
-    
-     arr.sort do |a,b|
-      a<=>b
-    end 
-    
-    arr[1] = 'ĉu vi parolas esperanton'
-else
-  
-
- 
-  arr.sort do |a,b|
-    a<=>b
-  end  
-
-end
-
+ if arr = 
 end
